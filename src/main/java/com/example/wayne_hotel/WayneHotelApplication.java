b77274c93ea4d8e06550f6d13f33f988f3d6078f
Fazliddin <dilshodovfazliddin@gmail.com>
2023-06-27 11:22:28 +0500

    first commit

---
+++ src/main/java/com/example/wayne_hotel/WayneHotelApplication.java
@@ -0,0 +1,13 @@
+package com.example.wayne_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayneHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WayneHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wayne_hotel/WayneHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wayne_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayneHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
